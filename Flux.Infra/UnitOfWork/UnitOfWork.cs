43e14bb62e0efe3cccfaad2c2ef55f0acf30ba84
Thiago Bressanin <thiago.bressanin@iterative.com.br>
2019-04-25 18:12:19 -0300

    UnitOfWork classes

---
+++ Flux.Infra/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flux.Infra.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public readonly IUnitOfWork _unitOfWork;
+
+        public UnitOfWork(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
